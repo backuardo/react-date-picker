b3159d9741b6af982031407f74d83b983b01b0c0
cameoapp <benjamin.eisner.20@dartmouth.edu>
2019-02-05 17:55:28 -0500

    selecting start and end, getStyle

--- src/components/DatePicker.js
+++ src/components/DatePicker.js
@@ -1,18 +1,21 @@
 import React, { Component } from "react";
+import Day from "./Day";
 import getWeeklyData from "../lib/getWeeklyData";
 import { MONTHS, DAYS } from "../constants";
 import "./DatePicker.css";
+
+// TODO: track which <td> the mouse is on to style the range
 
 class DatePicker extends Component {
   state = {
     start: null, // selected start date
     end: null, // selected end date
     now: new Date(), // current date
-    sel: new Date() // user's selected timeframe
+    sel: new Date() // user's selected timeframe, offset from `now`
   };
 
   // diff is the change in month -- should either be 1 or -1 ¯\_(ツ)_/¯
-  updateMonth = diff => {
+  updateSel = diff => {
     const newSel = this.state.sel;
     const currSelMonth = this.state.sel.getMonth();
     newSel.setMonth(currSelMonth + diff);
@@ -21,11 +24,48 @@ class DatePicker extends Component {
     console.log(getWeeklyData(this.state.sel));
   };
 
+  // set start and end time
+  // TODO: make sure start < end
+  updateStartOrEnd = day => {
+    const month = this.state.sel.getMonth();
+    const year = this.state.sel.getFullYear();
+    const date = new Date(year, month, day);
+
+    // check if we should set start or end date
+    if (!this.state.start || (this.state.start && this.state.end)) {
+      this.setState({ start: date, end: null });
+    } else {
+      this.setState({ end: date });
+    }
+  };
+
+  // get style for date
+  // TODO: handle "in range"
+  getDateStyle = day => {
+    const date = new Date(
+      this.state.sel.getFullYear(),
+      this.state.sel.getMonth(),
+      day
+    );
+
+    if (this.state.start && date.getTime() === this.state.start.getTime()) {
+      return "selected";
+    }
+    if (this.state.end && date.getTime() === this.state.end.getTime()) {
+      return "selected";
+    }
+    if (date < this.state.now) {
+      return "past";
+    }
+
+    return "";
+  };
+
   render() {
     return (
       <div className="container">
-        <button onClick={e => this.updateMonth(-1)}>-</button>
-        <button onClick={e => this.updateMonth(1)}>+</button>
+        <button onClick={e => this.updateSel(-1)}>-</button>
+        <button onClick={e => this.updateSel(1)}>+</button>
         <table className="calendar">
           {/*MO, YR*/}
           <caption>
@@ -34,23 +74,21 @@ class DatePicker extends Component {
           <tbody>
             {/* Su Mo Tu We Th Fr Sa */}
             <tr>
-              {DAYS.map(day => {
-                return <td key={day}>{day}</td>;
-              })}
+              {DAYS.map(day => (
+                <td key={day}>{day}</td>
+              ))}
             </tr>
             {/* days in the month */}
             {getWeeklyData(this.state.sel).map(week => (
               <tr key={week[0][1]}>
-                {week.map((day, i) => {
-                  {
-                    /*make this td its own component*/
-                  }
-                  return (
-                    <td key={i}>
-                      <span>{day[1]}</span>
-                    </td>
-                  );
-                })}
+                {week.map((day, i) => (
+                  <Day
+                    key={i}
+                    day={day[1]}
+                    selectDate={this.updateStartOrEnd}
+                    status={this.getDateStyle(day[1])}
+                  />
+                ))}
               </tr>
             ))}
           </tbody>
---
+++ src/components/Day.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "./Day.css";
+
+class Day extends Component {
+  render() {
+    return (
+      <td onClick={e => this.props.selectDate(this.props.day)}>
+        <span className={this.props.status}>{this.props.day}</span>
+      </td>
+    );
+  }
+}
+
+export default Day;
