ed036d02af0249a97e98da84a248a5c4e15c6ca6
cameoapp <benjamin.eisner.20@dartmouth.edu>
2019-02-05 18:37:50 -0500

    styling logic done for now

--- src/components/DatePicker.js
+++ src/components/DatePicker.js
@@ -4,14 +4,13 @@ import getWeeklyData from "../lib/getWeeklyData";
 import { MONTHS, DAYS } from "../constants";
 import "./DatePicker.css";
 
-// TODO: track which <td> the mouse is on to style the range
-
 class DatePicker extends Component {
   state = {
     start: null, // selected start date
     end: null, // selected end date
     now: new Date(), // current date
-    sel: new Date() // user's selected timeframe, offset from `now`
+    sel: new Date(), // user's selected timeframe, offset from `now`
+    hov: null // hovering date
   };
 
   // diff is the change in month -- should either be 1 or -1 ¯\_(ツ)_/¯
@@ -20,45 +19,66 @@ class DatePicker extends Component {
     const currSelMonth = this.state.sel.getMonth();
     newSel.setMonth(currSelMonth + diff);
     this.setState({ sel: newSel });
-
-    console.log(getWeeklyData(this.state.sel));
   };
 
   // set start and end time
-  // TODO: make sure start < end
   updateStartOrEnd = day => {
     const month = this.state.sel.getMonth();
     const year = this.state.sel.getFullYear();
     const date = new Date(year, month, day);
 
+    // check if date is in the past
+    if (date < this.state.now) {
+      return;
+    }
+
     // check if we should set start or end date
     if (!this.state.start || (this.state.start && this.state.end)) {
       this.setState({ start: date, end: null });
+    } else if (this.state.start && date < this.state.start) {
+      this.setState({ start: date });
     } else {
       this.setState({ end: date });
     }
   };
 
-  // get style for date
-  // TODO: handle "in range"
+  // update hovering date
+  updateHov = day => {
+    const month = this.state.sel.getMonth();
+    const year = this.state.sel.getFullYear();
+    const date = new Date(year, month, day);
+
+    // check if date is in the past
+    if (date < this.state.now) {
+      return;
+    }
+
+    this.setState({ hov: date });
+  };
+
+  // get className for given day
   getDateStyle = day => {
     const date = new Date(
       this.state.sel.getFullYear(),
       this.state.sel.getMonth(),
       day
     );
+    const dateTime = date.getTime();
+    const hovTime = this.state.hov && this.state.hov.getTime();
+    const startTime = this.state.start && this.state.start.getTime();
+    const endTime = this.state.end && this.state.end.getTime();
 
-    if (this.state.start && date.getTime() === this.state.start.getTime()) {
-      return "selected";
+    if (dateTime === startTime || dateTime === endTime) {
+      return "selected"; // selected start or end dates
+    } else if (startTime < dateTime && dateTime < endTime) {
+      return "range"; // between start and end dates
+    } else if (date < this.state.now) {
+      return "past"; // invalid selection
+    } else if (startTime && startTime < dateTime && dateTime < hovTime) {
+      return "range"; // between start and hover dates
+    } else {
+      return "";
     }
-    if (this.state.end && date.getTime() === this.state.end.getTime()) {
-      return "selected";
-    }
-    if (date < this.state.now) {
-      return "past";
-    }
-
-    return "";
   };
 
   render() {
@@ -86,6 +106,7 @@ class DatePicker extends Component {
                     key={i}
                     day={day[1]}
                     selectDate={this.updateStartOrEnd}
+                    hovering={this.updateHov}
                     status={this.getDateStyle(day[1])}
                   />
                 ))}
--- src/components/Day.js
+++ src/components/Day.js
@@ -4,7 +4,10 @@ import "./Day.css";
 class Day extends Component {
   render() {
     return (
-      <td onClick={e => this.props.selectDate(this.props.day)}>
+      <td
+        onClick={e => this.props.selectDate(this.props.day)}
+        onMouseOver={e => this.props.hovering(this.props.day)}
+      >
         <span className={this.props.status}>{this.props.day}</span>
       </td>
     );
